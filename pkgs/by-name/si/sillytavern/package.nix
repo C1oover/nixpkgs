a4de6497f9a1286b3d5408851163cfd3b5faa5e1
Elias Reimoser <elias.reimoser@gmail.com>
2024-05-30 16:46:06 +0200

    revert hash to v12-preview

--- pkgs/by-name/si/sillytavern/package.nix
+++ pkgs/by-name/si/sillytavern/package.nix
@@ -65,7 +65,7 @@ buildNpmPackage rec {
     owner = "SillyTavern";
     repo = "SillyTavern";
     rev = "${version}";
-    hash = "sha256-L9cJcRKow7VVu/NJlCIL4NseJJkzI+rOb6Qi4KXbb64=";
+    hash = "sha256-q63k7TNaGJEX4OQu4TNN06SfynI4vmpFHl5Dt5X11ms=";
   };
 
   npmDepsHash = "sha256-l142dONvY743+xEV/Ris/elLYh878/BjbJPW1zyY8M4=";
