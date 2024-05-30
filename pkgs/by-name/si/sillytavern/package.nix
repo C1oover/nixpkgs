c24359eca0bb18fe2f37870ef5b63690c6d57267
Elias Reimoser <elias.reimoser@gmail.com>
2024-05-30 17:31:35 +0200

    update to v12 and change nodejs to v20 stable

--- pkgs/by-name/si/sillytavern/package.nix
+++ pkgs/by-name/si/sillytavern/package.nix
@@ -60,19 +60,21 @@ in
 
 buildNpmPackage rec {
   pname = "sillytavern";
-  version = "1.11.5";
+  version = "1.12.0";
   src = fetchFromGitHub {
     owner = "SillyTavern";
     repo = "SillyTavern";
     rev = "${version}";
-    hash = "sha256-ErTDqn/PhoempJvOIcPHTcT2jEpCJxnRbUW/4tos94M=";
+    hash = "sha256-L9cJcRKow7VVu/NJlCIL4NseJJkzI+rOb6Qi4KXbb64=";
   };
 
-  npmDepsHash = "sha256-InOI5A6NuuMpadH5KIJUDPisvN2gjXxZonpo0Y/V8RA=";
+  npmDepsHash = "sha256-l142dONvY743+xEV/Ris/elLYh878/BjbJPW1zyY8M4=";
 
   desktopFile = ./sillytavern.desktop;
 
   dontNpmBuild = true;
+
+  nodejs = pkgs.nodejs_20
 
   nativeBuildInputs = [ makeWrapper ];
 
